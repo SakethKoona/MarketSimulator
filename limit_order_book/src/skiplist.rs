87477b8cbe1c0f292f27987dab3c721b073ea792
Saketh <koona.saketh@gmail.com>
2025-12-04 03:49:57 -0500

    Implemented search method, todo still are insert, delete, and thhen finish wrapper for PriceLevel struct, to finally get started on Order Book implementation

--- limit_order_book/src/skiplist.rs
+++ limit_order_book/src/skiplist.rs
@@ -1,14 +1,19 @@
 use crate::types::PriceLevel;
 
+const MAX_HEIGHT: usize = 24;
 
 struct SkipListNode<T> {
     value: T,
-    next: Vec<Option<Box<SkipListNode<T>>>>,
+    next: [Option<Box<SkipListNode<T>>>; MAX_HEIGHT],
+    height: usize,
 }
 
 struct SkipList<T: Ord> {
     pub head: Box<SkipListNode<T>>,
     pub max_height: usize,
+
+    // Private Variables
+    length: usize,
 }
 
 impl<T: Ord> SkipList<T> {
@@ -18,11 +23,44 @@ impl<T: Ord> SkipList<T> {
         return SkipList::<T> {
             head: head,
             max_height: Self::MAX_HEIGHT,
+            length: 0,
         };
     }
 
-    pub fn search(&self, node: T) {
-        todo!("Implement the search traversal for skiplist");
+    pub fn search(&self, node: T) -> Option<&SkipListNode<T>> {
+        // First, we get the head node
+        let mut current = &*self.head;
+
+        // Starts at the head node, at the topmost level, then keeps going down until we are at the
+        // breaking point between the target and before, or as close as possible without being
+        // greater than it.
+        for level in (0..self.max_height).rev() {
+            while let Some(ref next_node) = current.next[level] {
+                if next_node.value == node {
+                    return Some(next_node);
+                } else if next_node.value < node {
+                    current = next_node;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        // The next part, now that we got as close as possible descending the levels on level 0 to
+        // the node, we just do a regular linked list traversal to find the target or return None.
+        while let Some(ref next_node) = current.next[0] {
+            if next_node.value == node {
+                return Some(next_node);
+            } else {
+                current = next_node;
+            }
+        }
+
+        None
+    }
+
+    pub fn len(&mut self) -> usize {
+        return self.length;
     }
 
     pub fn insert(&self, node: T) {
@@ -37,13 +75,9 @@ impl<T: Ord> SkipList<T> {
 impl SkipList<PriceLevel> {
     //! This function searches by the specific price level, and is only relevant for the PriceLevel
     //! version of this generic, it doesn't apply to all generic parameters T.
-    pub fn search_by_price(&self, price: u32) {
-        
-        // First, we start at the highest level
-        let starting_node = &self.head.next[0].expect("[FATAL ERROR]: Head Node is empty");
+    pub fn search_by_price(&self, price: u32) -> Option<PriceLevel> {
+        // First, we start at the head, which will have pointers to all levels up to max_height
 
-        while starting_node.next.is_empty() == false {
-            todo!();
-        }
+        None
     }
 }
