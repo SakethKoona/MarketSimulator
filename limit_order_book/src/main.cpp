f0709a191618636d0fb78653b0a6dc33011a76ea
Saketh <koona.saketh@gmail.com>
2025-12-18 01:23:07 -0500

    initial orderbook tests ran successfully, add mroe complex tests and finish method to display orderbook todo

--- limit_order_book/src/main.cpp
+++ limit_order_book/src/main.cpp
@@ -2,11 +2,28 @@
 #include <iostream>
 #include <ostream>
 
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
 
   OrderBook ob{};
 
-  Order exampleOrder = Order(1, 2399, 5, OrderType::LIMIT, TypeInForce::GTC, Side::Buy);
-  ob.addOrder(exampleOrder);
+  Order exampleOrder =
+      Order(1, 2399, 5, OrderType::LIMIT, TypeInForce::GTC, Side::Buy);
+  auto result = ob.addOrder(exampleOrder);
+  switch (result) {
+  case OrderResult::Success:
+    std::cout << "YES, IT FUCKING WORKS" << std::endl;
+    break;
+  default:
+    std::cout << "WE FAILED :(" << std::endl;
+  }
+
+  switch (ob.cancelOrder(2)) {
+  case OrderResult::Success:
+    std::cout << "WORKED AGAIN HAHA" << std::endl;
+    break;
+  default:
+    std::cout << "WE DID SOMETHING WRONG" << std::endl;
+  }
+
   return 0;
 }
