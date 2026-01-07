70a8af44a3e2b09fbe66f3d4f0e721041826e15f
Saketh <koona.saketh@gmail.com>
2026-01-07 02:22:50 -0500

    pushng just this main file changes

--- limit_order_book/src/main.cpp
+++ limit_order_book/src/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 #include <thread>
 
+void worker() {
+    // Sample worker thread that just prints someting out
+
+    std::cout << "Printing from Worker thread" << std::endl;
+}
+
 int main() {
     std::cout << "Event Emission Testing" << std::endl;
     std::cout << "Part 1" << std::endl;
+
+    std::thread t(worker);
+    t.join();
+
+    std::cout << "printing from main" << std::endl;
 }
