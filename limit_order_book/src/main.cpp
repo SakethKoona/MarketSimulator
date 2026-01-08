ec1cd289808d592087f6052e173cb51216d3fb7c
Saketh <koona.saketh@gmail.com>
2026-01-07 22:58:16 -0500

    starting the implementation of the event emitter

--- limit_order_book/src/main.cpp
+++ limit_order_book/src/main.cpp
@@ -3,8 +3,11 @@
 
 void worker() {
     // Sample worker thread that just prints someting out
-
     std::cout << "Printing from Worker thread" << std::endl;
+}
+
+void worker2() {
+    std::cout << "Printing from the second worker thread" << std::endl;
 }
 
 int main() {
@@ -12,7 +15,9 @@ int main() {
     std::cout << "Part 1" << std::endl;
 
     std::thread t(worker);
+    std::thread t2(worker2);
     t.join();
+    t2.join();
 
     std::cout << "printing from main" << std::endl;
 }
